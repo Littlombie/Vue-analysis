8e77c0882c67ac4f2cad6778b016209ecad75efb
Littlombie <deanwen258@outlook.com>
2018-10-28 03:11:55 +0800

    update deploy.sh settings

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:ustbhuangyi/vue-analysis.git master
+git push -f git@github.com:Littlombie/Vue-analysis.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
